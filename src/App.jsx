d9f1c252802f528f7db95f89f34a19ab6b60bcbb
canzitis <maks.zelenin22@gmail.com>
2021-09-06 15:00:12 +0300

    added mobile site

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import { Route } from "react-router-dom";
 import SearchPageContainer from "./Components/SearchPage/SearchPageContainer";
 import SearchResultPageContainer from "./Components/SearchResultPage/SearchResultPageContainer";
 import ProfileBooksContainer from "./Components/ProfileBooks/ProfileBooksContainer";
+import logoGit from "./img/github.png";
+import logoVk from "./img/vk.png";
+import logoInsta from "./img/instagram.png";
+import bookImg from "./img/book-black.png";
 
 class App extends React.Component {
   render() {
@@ -14,6 +18,23 @@ class App extends React.Component {
           <Route path="/search" render={() => <SearchPageContainer />} />
           <Route path="/search" render={() => <SearchResultPageContainer />} />
           <Route path="/book/:id?" render={() => <ProfileBooksContainer />} />
+        </div>
+        <div className="boxSocialNetworks">
+          <div className="box-social">
+            <a target="blank" href="https://github.com/canzitis">
+              <img src={logoGit} alt="" />
+            </a>
+            <a  className="img2" target="blank" href="https://vk.com/canzit">
+              <img src={logoVk} alt="" />
+            </a>
+            <a target="blank" href="https://www.instagram.com/canzit">
+              <img src={logoInsta} alt="" />
+            </a>
+          </div>
+        </div>
+        <div className="copyright">
+          <span>© 2021 Search for books, Inc</span>
+          <img src={bookImg} alt="" />
         </div>
       </div>
     );
